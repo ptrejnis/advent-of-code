a0dcf4139b7f3aa5879038ea6bdc7998a2961b81
Przemysław Trejnis <ptrejnis@MacBook-Pro-Przemysaw.local>
2022-04-16 21:54:14 +0200

    Format Day 2 solutions

--- src/d2/index.ts
+++ src/d2/index.ts
@@ -25,15 +25,13 @@ const getCoords = (move: string): Course => {
   return { position, course };
 };
 
-const getHorizontal = (courses: Course[]): Position['horizontal'][] => {
-  return courses.filter(({ position }) => position === 'forward').map(({ course }) => course);
-};
+const getHorizontal = (courses: Course[]): Position['horizontal'][] =>
+  courses.filter(({ position }) => position === 'forward').map(({ course }) => course);
 
-const getDepths = (courses: Course[]): Position['depth'][] => {
-  return courses
+const getDepths = (courses: Course[]): Position['depth'][] =>
+  courses
     .filter(({ position }) => position === 'up' || position === 'down')
     .map(({ position, course }) => (position === 'down' ? course * -1 : course));
-};
 
 const calculatePosition = (courseChanges: number[]): number => courseChanges.reduce((curr, acc) => acc + curr, 0);
 
