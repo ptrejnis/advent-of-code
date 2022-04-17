5a63ae031189a6048e5b1adf4d9e1158bc0610b5
Przemysław Trejnis <ptrejnis@MacBook-Pro-Przemysaw.local>
2022-04-17 13:39:38 +0200

    Clean up challenge from task D2

--- src/d2/index.ts
+++ src/d2/index.ts
@@ -2,18 +2,11 @@ const exampleCourse = ['forward 5', 'down 5', 'forward 8', 'up 3', 'down 8', 'fo
 
 type Move = 'forward' | 'up' | 'down';
 
-function getResult(moves: string[]): number {
-  const coords = moves.map(getCoords);
-
-  return calculate(coords);
-}
+const getResult = (moves: string[]): number => calculate(moves.map(getCoords));
 
 const calculate = (coords: [Move, number][]): number => {
-  const coordsMap: Map<Move, number> = new Map([
-    getCoordsEntry(coords, 'forward'),
-    getCoordsEntry(coords, 'up'),
-    getCoordsEntry(coords, 'down'),
-  ]);
+  const moveOptions: Move[] = ['forward', 'up', 'down'];
+  const coordsMap: Map<Move, number> = new Map(moveOptions.map((move) => getCoordsEntry(coords, move)));
   const horizontalTotal = coordsMap.get('forward');
   const depth = coordsMap.get('up') + coordsMap.get('down') * -1;
   const depthTotal = depth < 0 ? depth * -1 : depth;
@@ -28,6 +21,7 @@ const getCoordsEntry = (coords: [Move, number][], move: Move): [Move, number] =>
     .map(([, coords]) => coords)
     .reduce((curr, acc) => acc + curr, 0),
 ];
+
 const getCoords = (move: string): [Move, number] => {
   const [position, course] = move.split(' ');
 
@@ -35,4 +29,5 @@ const getCoords = (move: string): [Move, number] => {
     return [position, parseFloat(course)];
   }
 };
+
 export const solution = getResult(exampleCourse);
